e252db29fdc0397e51eb059ecd1dcd2caec9f8b0
BSReis <75648762+BSReis@users.noreply.github.com>
2022-02-04 01:22:17 +0000

    Added new pages and fixed top and bottom nav

--- main/gestors.js
+++ main/gestors.js
@@ -1,14 +1,14 @@
-// {
-//     const nav = document.getElementById("main-navbar");
-//     let lastScrollY = window.scrollY;
+{
+    const nav = document.getElementById("bottomNav");
+    let lastScrollY = window.scrollY;
 
-//     window.addEventListener("scroll", () => {
-//         if (lastScrollY < window.scrollY) {
-//             nav.classList.add("navHidden");
-//         } else {
-//             nav.classList.remove("navHidden");
-//         }
+    window.addEventListener("scroll", () => {
+        if (lastScrollY < window.scrollY) {
+            nav.classList.add("navHidden");
+        } else {
+            nav.classList.remove("navHidden");
+        }
 
-//         lastScrollY = window.scrollY;
-//     });
-// }
+        lastScrollY = window.scrollY;
+    });
+}
